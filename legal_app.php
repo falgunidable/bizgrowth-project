b676b7e5748826a6048da830d993b996ae34673c
Falguni Dable <falgunidable@gmail.com>
2023-02-14 11:42:52 +0530

    functionality changes in legal app file

--- legal_app.php
+++ legal_app.php
@@ -6,10 +6,6 @@ include('Navbar/nav.php');
     *{
         font-family: 'Montserrat', sans-serif;
     }
-    .continue:hover{
-      background-color:#fe7f10 !important;;
-      color:white
-    }
 
     .progresses{
       display: flex;
@@ -19,7 +15,7 @@ include('Navbar/nav.php');
    .line{
       width: 40px;
       height: 6px;
-      background: lightgrey;;
+      background: lightgrey;
    }
 
    .steps{
@@ -49,27 +45,44 @@ include('Navbar/nav.php');
           <div style="background-color:#fe7f10;padding:10px">
             <h5 style="color:white"><b>Discover licences & solutions best-suited for your business</b></h5>
           </div><br/>
-          <h5><b>Select Stage</b></h5><br/>
-          <div class="d-flex justify-content-evenly">
-            <div style="cursor:pointer">
-              <img src="./images/legal1.svg" onclick="startBus(this);" id="start"/><br/><br/>
-              <h6 id="startText"><b>Starting a Business</b></h6>
-            </div>
-            <div style="cursor:pointer">
-              <img src="./images/legal2.svg" onclick="existBus(this);" id="exist"/>
-              <br/><br/><h6 id="existText"><b>Existing Business</b></h6>
-            </div>
-          </div><br/>
-          <button class="p-2 disabled border-0" disabled><b>Continue</b></button><br/><br/>
+          <div id="selecBusi">
+            <h5><b>Select Stage</b></h5><br/>
+            <div class="d-flex justify-content-evenly">
+              <div style="cursor:pointer">
+                <img src="./images/legal1.svg" onclick="startBus(this);" id="start"/><br/><br/>
+                <h6 id="startText"><b>Starting a Business</b></h6>
+              </div>
+              <div style="cursor:pointer">
+                <img src="./images/legal2.svg" onclick="existBus(this);" id="exist"/>
+                <br/><br/><h6 id="existText"><b>Existing Business</b></h6>
+              </div>
+            </div><br/>
+            <button id="continueBtn" class="p-2 border-0" onclick="nextStep()" disabled><b>Continue</b></button><br/><br/>
+          </div>
+          <div id="selecBusiNext" style="display:none">
+            <h5><b>Do you know what you are looking for?</b></h5><br/>
+            <div class="d-flex justify-content-evenly">
+              <div style="cursor:pointer">
+                <img src="./images/legal1.svg" onclick="startBus(this);" id="start"/><br/><br/>
+                <h6 id="startText"><b>Starting a Business</b></h6>
+              </div>
+              <div style="cursor:pointer">
+                <img src="./images/legal2.svg" onclick="existBus(this);" id="exist"/>
+                <br/><br/><h6 id="existText"><b>Existing Business</b></h6>
+              </div>
+            </div><br/>
+            <button id="continueBtn" class="p-2" style="border:1px solid #fe7f10;background-color:white"><b>Back</b></button>  
+            <button id="continueBtn" class="p-2 disabled border-0" disabled><b>Continue</b></button><br/><br/>
+          </div>
           <div class="container d-flex justify-content-center align-items-center">
             <div class="progresses">
-              <div class="steps"></div>
-              <span class="line"></span>
-              <div class="steps"></div>
-              <span class="line"></span>
-              <div class="steps"></div>
-              <span class="line"></span>
-              <div class="steps"></div>
+              <div class="steps" style="background-color: greenyellow"></div>
+              <span class="line" id="line1"></span>
+              <div class="steps" id="step1"></div>
+              <span class="line" id="line2"></span>
+              <div class="steps" id="step2"></div>
+              <span class="line" id="line3"></span>
+              <div class="steps" id="step3"></div>
             </div>    
           </div>
           <br/>
@@ -84,12 +97,22 @@ include('Navbar/nav.php');
     document.getElementById('exist').src = './images/legal2.svg';
     document.getElementById('startText').style.color = '#6348b5';
     document.getElementById('existText').style.color = 'black';
+    document.getElementById('continueBtn').disabled = false;
+    document.getElementById('continueBtn').style.backgroundColor='#fe7f10';
+    document.getElementById('continueBtn').style.color='white';
   }
   function existBus(element) {
     element.setAttribute('src', './images/legalhover2.svg');
     document.getElementById('start').src = './images/legal1.svg';
     document.getElementById('existText').style.color = '#6348b5';
     document.getElementById('startText').style.color = 'black';
+    document.getElementById('continueBtn').disabled = false;
+  }
+  function nextStep(){
+    document.getElementById('selecBusi').style.display = 'none';
+    document.getElementById('selecBusiNext').style.display = 'block';
+    document.getElementById('step1').style.backgroundColor = 'greenyellow';
+    document.getElementById('line1').style.backgroundColor = 'greenyellow';
   }
 </script>
 <?php
